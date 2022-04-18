b9b8e0cde7c35a6b9ba40e291f1557feae147c03
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-04-18 08:29:20 -0600

    Feat: create and initialize Activity class

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,1 +1,12 @@
 # For the `add_participant` method, the first argument represents the name of someone who participated in the activity, and the second argument represents how much they paid for the activity.
+
+class Activity
+  attr_reader :name,
+              :participants
+
+  def initialize(name)
+    @name = name
+    @participants = {}
+  end
+
+end
