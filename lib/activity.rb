caa8966eb873b19bf418255c99b5000210a1c711
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-04-18 19:06:30 -0600

    Test: add Activity can show amount owed from each participant

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,5 +1,3 @@
-# For the `add_participant` method, the first argument represents the name of someone who participated in the activity, and the second argument represents how much they paid for the activity.
-
 class Activity
   attr_reader :name,
               :participants
@@ -10,7 +8,7 @@ class Activity
   end
 
   def add_participant(participant, cost)
-    @participants = {participant => cost}
+    @participants[participant] = cost
   end
 
   def total_cost
@@ -22,3 +20,8 @@ class Activity
   end
 
 end
+
+
+# For the `split` method, it is assumed that the cost is evenly distributed amongst participants. It should be the total cost divided by the number of participants.
+
+# For the `owed` method, the amount each person owes is the difference between what they paid and the `split`. If a participant paid less than their fair share they owe a positive amount. If a participant paid more than their fair share they owe a negative amount (meaning they are owed money).
--- spec/activity_spec.rb
+++ spec/activity_spec.rb
@@ -19,9 +19,31 @@ describe Activity do
     expect(activity.participants).to eq({"Maria" => 20})
   end
 
-  it "can return total cost of participant" do
+  it "can return cost of one participant" do
     activity.add_participant("Maria", 20)
 
     expect(activity.total_cost).to eq(20)
+  end
+
+  it "can return total cost of all participant" do
+    activity.add_participant("Maria", 20)
+    activity.add_participant("Luther", 40)
+
+    expect(activity.participants).to eq({"Maria" => 20, "Luther" => 40})
+    expect(activity.total_cost).to eq(60)
+  end
+
+  it "can split total cost evenly among all participant" do
+    activity.add_participant("Maria", 20)
+    activity.add_participant("Luther", 40)
+
+    expect(activity.split).to eq(30)
+  end
+
+  xit "can show amount owed from each participant" do
+    activity.add_participant("Maria", 20)
+    activity.add_participant("Luther", 40)
+
+    expect(activity.owed).to eq({"Maria" => 10, "Luther" => -10})
   end
 end
